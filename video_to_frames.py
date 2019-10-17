6b5997215bc77fa455db9a4420404b187610c229
Vladislav Plotnikov <vladik1209@gmail.com>
2019-10-17 23:17:43 +0300

    Some fixes to improve usability

--- video_to_frames.py
+++ video_to_frames.py
@@ -1,16 +1,27 @@
+# This script made by Vladislav Plotnikov (vladik1209@gmail.com)
+# This script transforms your video into number of pictures. Pictures from your video produces every second 
+# To run this script run the following command in bash terminal 
+#
+# $ python [path_to_your_work_directory]/video_to_frames.py [path_to_your_video] [framerate] [picture_filename] [format]
+#
+# To take more pictures for each second, just divide [framerate] parameter. 
+# [format] parameter can be: jpg, png and etc. Write it without any dots!
+
 import sys
 import cv2 as cv
 
 print("Starting ...")
 if len (sys.argv) == 2:
-	print ("Please enter the framerate and destination filename!\nExiting ...")
+	print ("Please enter the framerate, destination filename and format!\nExiting ...")
 	sys.exit (1)
-
 elif len (sys.argv) == 3:
-	print ("Please enter the destination filename!\nExiting ...")
+	print ("Please enter the destination filename and format!\nExiting ...")
+	sys.exit (1)
+elif len (sys.argv) == 4:
+	print ("Please enter the destination format!\nExiting ...")
 	sys.exit (1)
 
-elif len (sys.argv) == 4:
+elif len (sys.argv) == 5:
 	cap = cv.VideoCapture(sys.argv[1])
 	print("Video is opened: %s" % (cap.isOpened()))
 	i = 0
@@ -18,12 +29,15 @@ elif len (sys.argv) == 4:
 	while cap.isOpened():
 	    ret, frame = cap.read()
 	    # if frame is read correctly ret is True
-	    if not ret:
-	        print("Can't receive frame (stream end?). Exiting ...")
+	    if (not ret and img_counter == 0):
+	        print("Can't receive frame. Exiting ...")
+	        break
+	    elif (not ret and img_counter > 0):
+	        print("Finish!")
 	        break
 	    if i == int(sys.argv[2]):
-	    	#print("FUK UUU!!")
-	    	cv.imwrite('%s%d.png' %(sys.argv[3], img_counter),frame)
+	    	print('%s%d.%s' %(sys.argv[3], img_counter, sys.argv[4]))
+	    	cv.imwrite('%s%d.%s' %(sys.argv[3], img_counter, sys.argv[4]),frame)
 	    	img_counter += 1
 	    	i = 0
 	    else:
@@ -35,4 +49,4 @@ elif len (sys.argv) == 4:
 	cap.release()
 	cv.destroyAllWindows()
 else:
-	print ("Please enter the path to video file, framerate and destination filename!\nExiting ...")
+	print ("Please enter the path to video file, framerate, destination filename and format!\nExiting ...")
